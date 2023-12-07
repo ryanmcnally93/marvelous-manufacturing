c2f2e8646f3cd91e81666e3208362fc75f819422
Ryan McNally <ryanmcnallymusic@outlook.com>
2023-12-07 21:11:30 +0000

    Add areas pages

---
+++ assets/js/index.js
@@ -0,0 +1,17 @@
+function changeAreaImage(area) {
+    area.style.display = 'flex';
+    let type = area.id;
+    let image = document.getElementById(type + '-image');
+    // image.style.display = 'none';
+    area.style.cursor = 'pointer';
+    area.style.border = '1px solid blue';
+    area.style.backgroundColor = 'white';
+    area.style.opacity = '0.8';
+}
+
+function changeAreaImageBack(area) {
+    area.style.display = 'none';
+    let type = area.id;
+    let image = document.getElementById(type + '-image');
+    image.style.display = 'flex';
+}
