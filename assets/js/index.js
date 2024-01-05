927d45e8d3fef3c74315cb646320ef6192fb9b0d
Ryan McNally <ryanmcnallymusic@outlook.com>
2024-01-05 17:59:59 +0000

    Add collapsibles to news page

--- assets/js/index.js
+++ assets/js/index.js
@@ -15,3 +15,8 @@ function changeAreaImageBack(area) {
     let image = document.getElementById(type + '-image');
     image.style.display = 'flex';
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+    let collapsibles = document.querySelectorAll('.collapsible');
+    M.Collapsible.init(collapsibles);
+});
